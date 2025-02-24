30c608d587824c46ddad7911e05e987ed61d5671
Kseniia Bondarenko <ksen9291@gmail.com>
2025-02-24 13:34:45 -0800

    progress-2.12pm-Feb-24-2025

--- src/about-and-contact/DefaultSetup.jsx
+++ src/about-and-contact/DefaultSetup.jsx
@@ -12,24 +12,24 @@ export default function DefaultSetup() {
 
 
         <div className="DefaultSetup">
-            <div>
+            <div className="DefaultSetup-logos">
                 <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                    <img src={viteLogo} className="DefaultSetup-vite-logo" alt="Vite logo" />
                 </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
+                <a className="DefaultSetup-link" href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="DefaultSetup-react-logo" alt="React logo" />
                 </a>
             </div>
             <h1>Vite + React</h1>
-            <div className="card">
+            <div className=".DefaultSetup-card">
                 <button onClick={() => setCount((count) => count + 1)}>
                     count is {count}
                 </button>
-                <p>
+                <p className="DefaultSetup-note">
                     Edit <code>src/App.jsx</code> and save to test HMR
                 </p>
             </div>
-            <p className="read-the-docs">
+            <p className="DefaultSetup-read-the-docs">
                 Click on the Vite and React logos to learn more
             </p>
         </div>
