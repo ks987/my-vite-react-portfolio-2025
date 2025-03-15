9f5fa6f3959b65ec2b0a757d1fa0b01c08665cec
Kseniia Bondarenko <ksen9291@gmail.com>
2025-03-15 08:57:11 -0700

    feat: Add responsive design styles

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import { Link, Outlet } from 'react-router-dom';
-
-
-// Import components
-import Footer from './Footer.jsx';
-
+import { Link } from 'react-router-dom';
 
 
 // import css files 
