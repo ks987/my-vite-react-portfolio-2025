3a996404d5e02b08af5456e136950476f2fc7967
Kseniia Bondarenko <ksen9291@gmail.com>
2025-04-11 12:14:11 -0700

    12.14pm-Apr-11-2025

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,4 @@
 
-// import React from "react";
-// import { StrictMode } from 'react';
-// import { HashRouter } from "react-router-dom";
-// import { createRoot } from 'react-dom/client';
-
-// import './index.css';
-// import App from './App.jsx';
-
-// createRoot(document.getElementById('root')).render(
-
-//   <StrictMode>
-//     <HashRouter>
-//       <App />
-//     </HashRouter>
-//   </StrictMode>
-// );
-
-
 import React from "react";
 import { StrictMode } from "react";
 import { HashRouter } from "react-router";
@@ -25,19 +7,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-console.log("✅ React is trying to mount..."); // Debugging Log
 
-const rootElement = document.getElementById("root");
 
-if (rootElement) {
-  console.log("✅ Found #root, mounting React...");
-  createRoot(rootElement).render(
-    <StrictMode>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </StrictMode>
-  );
-} else {
-  console.error("❌ ERROR: #root not found! React cannot mount.");
-}
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()]
-// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // base: '/portfolio/react-portfolio-2025/my-vite-react-portfolio-2025/',
-  plugins: [react()],
+  base: '/portfolio/react-portfolio-2025/my-vite-react-portfolio-2025/',
+  plugins: [react()]
 });
 
 
