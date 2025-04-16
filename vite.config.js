5ceb9711be583df545caa1612500c7b45a0dfe98
Kseniia Bondarenko <ksen9291@gmail.com>
2025-04-16 15:19:35 -0700

    3.19pm-Apr-16-2025

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/portfolio/my-vite-react-portfolio-2025/'
 });
+
+
 
 
